14a22353244330fb835f86634d4ed6b5d0d2213c
ChrBarth <ChrBarth@users.noreply.github.com>
2017-07-10 09:53:05 +0200

    removed print-statements, tracks and albums now share one function

--- lastfmcharts.py
+++ lastfmcharts.py
@@ -37,45 +37,36 @@ for a in sys.argv[1:]:
 	if m:
 		if m.group(1) == "u":
 			lastfmuser = m.group(2)
-			print("user changed to " + lastfmuser)
 		if m.group(1) == "c":
 			cmatch = re.match("(artists|tracks|albums)", m.group(2))
 			if cmatch:
 				charttype = m.group(2)
-				print("charttype changed to " + charttype)
 		if m.group(1) == "f":
 			cmatch = re.match("\d\d\d\d-\d\d-\d\d", m.group(2))
 			if cmatch:
 				datefrom = m.group(2)
-				print("start date changed to " + datefrom)
 		if m.group(1) == "t":
 			cmatch = re.match("\d\d\d\d-\d\d-\d\d", m.group(2))
 			if cmatch:
 				dateto = m.group(2)
-				print("end date changed to " + dateto)
 		if m.group(1) == "l":
 			cmatch = re.match("^\d+$", m.group(2))
 			if cmatch:
 				limit = int(m.group(2))
-				print("limit changed to " + str(limit))
 		if m.group(1) == "s":
 			cmatch = re.match("^[yn]$", m.group(2), flags=re.IGNORECASE)
 			if cmatch:
 				if cmatch.group(0).lower() == "y":
 					display_scrobbles = True
-					print("Displaying scrobbles...")
 				else:
 					display_scrobbles = False
-					print("Don`t show scrobbles...")
 		if m.group(1) == "n":
 			cmatch = re.match("^[yn]$", m.group(2), flags=re.IGNORECASE)
 			if cmatch:
 				if cmatch.group(0).lower() == "y":
 					display_ranknums = True
-					print("Displaying rank numbers...")
 				else:
 					display_ranknums = False
-					print("Don`t display rank numbers...")
 
 if(lastfmuser == ""):
 	print("Need at least a last.fm username. Aborting...")
@@ -95,8 +86,8 @@ if charttype == "artists":
 		artist = a.span.text
 		result.append(artist.strip("\n"))
 
-# albums:
-if charttype == "albums":
+# albums/tracks:
+if charttype == "albums" or charttype == "tracks":
 	for a in soup.find_all("a", "link-block-target"):
 		if(a.get('title')):
 			result.append(a['title'])
@@ -104,29 +95,16 @@ if charttype == "albums":
 			result.append("unknown")
 			# happens sometimes when the tag contains special characters...
 
-# tracks:
-if charttype == "tracks":
-	for a in soup.find_all("a", "link-block-target"):
-		if(a.get('title')):
-			result.append(a['title'])
-		else:
-			result.append("unknown")
-			# see above
-
 # number of scrobbles:
 for s in soup.find_all("td", "chartlist-countbar"):
 	scrobble = s.span.text
 	scrobbles.append(int(scrobble.strip("\n Scrobles")))
 
-# adjust value of limit if we have fewer results than limit:
-if len(scrobbles)<limit:
-	limit = len(scrobbles)
-
 # trim the results:
 del result[limit:]
 
 # printing the charts:
-print("\n\nDisplaying %s-charts for user %s between %s and %s\n\n" % (charttype, lastfmuser, datefrom, dateto))
+print("%s for user %s between %s and %s\n" % (charttype, lastfmuser, datefrom, dateto))
 for n,line in enumerate((result)):
 	output = ""
 	if display_ranknums:
